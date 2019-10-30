2df0c91cb731965b6bb45a36b31ee3db0400ed75
Alexander Fedorets <alexander.fedorets@bestseller.com>
2019-10-30 16:00:32 +0100

    Make  legendary item abstract

--- src/main/java/com/gildedrose/items/LegendaryItem.java
+++ src/main/java/com/gildedrose/items/LegendaryItem.java
@@ -2,7 +2,7 @@ package com.gildedrose.items;
 
 import com.gildedrose.Item;
 
-public class LegendaryItem extends Item {
+public abstract class LegendaryItem extends Item {
     public LegendaryItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
